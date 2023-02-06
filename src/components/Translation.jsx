d1552409ddcfdb4de92310988acb5ab98159ae4e
Omeed Mariani <70778138+Mariani-code@users.noreply.github.com>
2023-02-05 22:02:23 -0500

    Update Translation.jsx

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Translation({ doStuff, setInput, result }) {
-  return (
-    <div>
-      <textarea
-        className="text-area"
-        cols={110}
-        rows={15}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
-      <button className="action-btn" onClick={doStuff}>
-        SUBMIT!
-      </button>
+  const [currentResult, setCurrentResult] = useState("");
+  const [text, setText] = useState("");
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setCurrentResult(result && result.slice(0, i));
+      setText(result ? text + result[i] : text);
+      i++;
+      if (!result || i >= result.length) clearInterval(interval);
+    }, 50);
+    return () => clearInterval(interval);
+  }, [result]);
 
-      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+  const AARP_blue = '#FF0000';
+  const AARP_yellow = '#FFFFFF';
+
+  return (
+    <div style={{ backgroundColor: 'white', display: "flex", flexDirection: 'column', alignItems: "center", height: '100vh', margin: 0 }}>
+      <img src={'https://1000logos.net/wp-content/uploads/2016/10/AARP-Logo.png'} style={{height: '100px', marginTop: '2em'}} />
+      <div style={{ width: "100%", maxWidth: '800px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <textarea
+          style={{ width: "100%", height: "10em", fontSize: "1.5em" }}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <br />
+        <button style={{ width: "100%", backgroundColor: AARP_blue, color: 'white', fontSize: "1.5em" }} onClick={doStuff}>
+          SUBMIT
+        </button>
+        <br />
+        <br />
+        <pre style={{ width: "100%", height: "10em", backgroundColor: AARP_yellow, whiteSpace: "pre-wrap", fontSize: "1.5em" }}>
+          {result ? text.slice(0, text.length - currentResult.length) : ''}
+          <mark style={{ backgroundColor: '#39FF14' }}>{result ? currentResult : ''}</mark>
+        </pre>
+      </div>
     </div>
   );
 }
