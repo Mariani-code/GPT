5503540728288f2c5916c9d2bc6b97152dc1531e
Omeed Mariani <70778138+Mariani-code@users.noreply.github.com>
2023-03-03 12:29:12 -0500

    Update Translation.jsx

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -64,7 +64,7 @@ export default function Translation({ doStuff, setInput, result }) {
         clearInterval(interval);
         setShowLoader(false);
       }
-    }, 10);
+    }, 1);
     // Clean up the interval when the component unmounts
     return () => clearInterval(interval);
   }, [result]);
@@ -92,99 +92,165 @@ return (
 
   <div style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'row', height: '100vh', margin: 0 }}>
     <div style={{ backgroundColor: 'white', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '80%' }}>
-      <div style={{ position: 'absolute', top: '1em', right: '1em' }}>
-        <img src={'https://1000logos.net/wp-content/uploads/2016/10/AARP-Logo.png'} style={{ height: '100px' }} />
+    <div style={{
+  backgroundImage: 'url(https://1000logos.net/wp-content/uploads/2016/10/AARP-Logo.png)',
+  backgroundSize: '8%',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 630,
+  backgroundColor: '#ffffff',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '150%',
+  height: '100%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '2em',
+  boxShadow: '0px 0px 20px rgba(0, 0, 0, 0.1)',
+  margin: 'auto',
+}}>
+
       </div>
       <h1 style={{ marginTop: '2em', color: 'red', fontFamily: 'Luminari, fantasy' }}>GPT Demonstrator</h1>
       <div style={{ width: '100%', maxWidth: '800px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <textarea
-          id="text-area"
-          style={{
-            width: '100%',
-            height: '200px',
-            fontSize: '1.5em',
-            padding: '1em',
-            borderRadius: '4px',
-            border: '1px solid black',
-            backgroundColor: 'lightgray',
-            boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.07)',
-            overflowY: 'scroll',
-            color: 'black',
-            fontWeight: 'bold',
-          }}
-          onChange={(e) => {
-            setInput(e.target.value);
-            setPreviousText(e.target.value);
-          }}
-        >
-          {previousText}
-        </textarea>
-        <input type="file" onChange={handleFileInput} />
-<div style={{position: 'relative', left: '-360px', top: '-20px'}}>
-<button style={{backgroundColor: AARP_red, color: 'white', fontSize: "1em"}} onClick={() => {setShowLoader(true); doStuff();}}>SUBMIT</button>
+      <textarea
+  id="text-area"
+  style={{
+    width: '100%',
+    height: '200px',
+    fontSize: '1.5em',
+    padding: '1em',
+    borderRadius: '4px',
+    border: '1px solid black',
+    backgroundColor: 'lightgray',
+    boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.07)',
+    overflowY: 'scroll',
+    color: 'black',
+    fontWeight: 'bold',
+  }}
+  onChange={(e) => {
+    setInput(e.target.value);
+    setPreviousText(e.target.value);
+  }}
+>
+  {previousText}
+</textarea>
+<button
+  style={{
+    marginLeft: '-57em',
+    marginTop: '1em',
+    backgroundColor: '#FF0000',
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: '30px',
+    border: 'none',
+    padding: '0.6em 1em',
+    cursor: 'pointer',
+  }}
+  onClick={() => {
+    setInput('');
+    setPreviousText('');
+    document.getElementById('text-area').value = '';
+  }}
+>
+  Clear
+</button>
+<br></br>
+<br></br>
+<input type="file" onChange={handleFileInput} />
+<div style={{position: 'relative', left: '350px', top: '-100px'}}>
+<button style={{backgroundColor: '#39FF14', color: 'white', fontSize: "1em", fontWeight: 'bold',borderRadius: '30px', border: 'none', padding: '0.5em 1em', cursor: 'pointer',}} onClick={() => {setShowLoader(true); doStuff();}}>SUBMIT</button>
 </div>
 <br />
 <br />
 <Tabs selectedIndex={selectedTabIndex}>
-<TabList>
+  <TabList>
+    {history.map((item, index) => (
+      <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+        <Tab key={index}>Session {history.length - index}</Tab>
+        <button style={{ marginLeft: '0.5em' }} onClick={() => { setSelectedTabIndex(index); }}>View</button>
+        {selectedTabIndex === index ? (
+          <button style={{ marginLeft: '0.5em' }} onClick={() => { setSelectedTabIndex(-1); }}>Close</button>
+        ) : null}
+      </div>
+    ))}
+  </TabList>
   {history.map((item, index) => (
-    <div key={index} style={{display: 'flex', alignItems: 'center'}}>
-      <Tab key={index}>Session {index + 1}</Tab>
-      <button style={{marginLeft: '0.5em'}} onClick={() => {setSelectedTabIndex(index);}}>View</button>
-      {selectedTabIndex === index ? (
-        <button style={{marginLeft: '0.5em'}} onClick={() => {setSelectedTabIndex(-1);}}>Close</button>
-      ) : null}
-    </div>
+    <TabPanel key={index}>
+      <div style={{ backgroundColor: "white", border: "1px solid #BFBFBF", borderRadius: "5px", padding: "0.5em", fontWeight: "bold", color: "#BFBFBF", fontSize: "1.2em", marginTop: "1em" }}>
+        <p><b>Input:</b> {item.input}</p>
+        <p><b>Output:</b> {item.output}</p>
+      </div>
+    </TabPanel>
   ))}
-</TabList>
-{history.map((item, index) => (
-  <TabPanel key={index}>
-    <div style={{backgroundColor: "white", border: "1px solid #BFBFBF", borderRadius: "5px", padding: "0.5em", fontWeight: "bold", color: "#BFBFBF", fontSize: "1.2em", marginTop: "1em"}}>
-      <p><b>Input:</b> {item.input}</p>
-      <p><b>Output:</b> {item.output}</p>
-    </div>
-  </TabPanel>
-))}
 </Tabs>
+
 <br />
 <br />
 <br />
-<pre style={{width: "200%", height: "50em", backgroundColor: 'white', whiteSpace: "pre-wrap", fontSize: "2em", overflowY: "hidden", textAlign: "center", margin: "0 auto", marginTop: "1em", marginBottom: "2em", marginLeft: "0em", marginRight: "-25em"}}>
-<p style={{color: 'black', display: showLoader ? 'block' : 'none'}}>Thinking...</p>
-<div style={{display: 'flex', justifyContent: 'center'}}>
-  {showLoader ? <BarLoader color="red"/> : null}
-</div>
-{text.slice(0, text.length - currentResult.length)}
-<mark style={{backgroundColor: '#39FF14'}}>{currentResult}</mark>
+<pre style={{
+  width: '200%',
+  maxWidth: '1300px',
+  height: '400px',
+  backgroundColor: '#333333',
+  color: 'white',
+  whiteSpace: 'pre-wrap',
+  fontSize: '2em',
+  overflowY: 'scroll',
+  textAlign: 'left',
+  margin: '0 auto',
+  marginTop: '1em',
+  marginBottom: '2em',
+  padding: '1em',
+  borderRadius: '4px',
+  boxShadow: 'inset 0 1px 2px rgba(0,0,0,0.07)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+}}>
+  <div style={{ color: 'white', textAlign: 'center', marginBottom: '1em', display: showLoader ? 'block' : 'none' }}>Thinking...</div>
+  <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '1em' }}>
+    {showLoader ? <BarLoader color="red" /> : null}
+  </div>
+  <span style={{ textAlign: 'left' }}>{result}</span>
 </pre>
+
+
+
+
+
+
+
+
+
+
       </div>
     </div>
-    <div style={{ backgroundColor: "#FFFFFF", display: "flex", flexDirection: 'column', width: '40%', height: '1vh', alignItems: "center", padding: "2em" }}>
-<h3 style={{ fontWeight: "bold", marginBottom: "1em", display: 'flex', alignItems: 'center' }}>
+    <div style={{ display: "flex", flexDirection: 'column', width: '40%', height: '1vh', alignItems: "center", padding: "2em" }}>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+    <h3 style={{ fontWeight: "bold", marginBottom: "1em", display: 'flex', alignItems: 'center' }}>
   Model
   <FontAwesomeIcon icon={faInfoCircle} style={{ marginLeft: '0.5em' }} />
   <Tooltip place="top" type="light" effect="solid" text="Select the language model to use for the translation." />
 </h3>
+
 <select style={{ width: '100%', padding: '1em', borderRadius: '4px', border: '1px solid #BBBBBB', backgroundColor: '#F2F2F2', fontWeight: "bold", marginBottom: "1.5em", color: '#000000' }}>
   <option value="gpt2">GPT-3</option>
   <option value="davinci">Davinci</option>
   <option value="curie">Curie</option>
   <option value="babbage">Babbage</option>
 </select>
-<h3 style={{ fontWeight: "bold", marginBottom: "1em", display: 'flex', alignItems: 'center' }}>
-  Temperature
-  <FontAwesomeIcon icon={faInfoCircle} style={{ marginLeft: '0.5em' }}
-    data-tip="Select the language model to use for the translation." />
-  <Tooltip />
-</h3>
-<input type="range" min="0" max="1" step="0.01" value={temperature} style={{ width: "100%", height: "2em", marginBottom: "1.5em", background: "#ff9a9e" }} onChange={handleTemperatureChange} />
-<h3 style={{ fontWeight: "bold", marginBottom: "1em", display: 'flex', alignItems: 'center' }}>
-  Maximum Length
-  <FontAwesomeIcon icon={faInfoCircle} style={{ marginLeft: '0.5em' }}
-    data-tip="Select the language model to use for the translation." />
-  <Tooltip />
-</h3>
-<input type="number" min="1" value={maxLength} style={{ width: "100%", padding: "0.5em", borderRadius: '4px', border: '1px solid #BBBBBB', backgroundColor: '#FFFFFF', fontWeight: "bold", color: '#000000'  }} onChange={handleMaxLengthChange} />
+
+
+
+
 </div>
 <Tooltip background="#333" />
   </div>
